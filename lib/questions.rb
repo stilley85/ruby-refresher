a0bc97d858b2601fccbbbcdaa59ad6cfc9c27aab
Stephen Tilley <stephentilley@gmail.com>
2018-03-12 00:27:11 +0000

    Completed most exercises

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,22 +1,27 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+  array.delete_if { |x| !x.start_with?('a') }
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.delete_if { |x| !x.start_with?('a', 'e', 'i', 'o', 'u') }
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.delete_if { |x| x == nil }
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.delete_if { |x| x == nil || x == false }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.each { |x| x.reverse! }
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
@@ -24,31 +29,51 @@ end
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice,
 def every_possible_pairing_of_students(array)
+  pairs = Array.new
+  for x in 0..array.length - 1
+    for y in x + 1..array.length - 1
+      pairs.push([array[x], array[y]])
+    end
+  end
+  return pairs
 end
 
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+    array.shift(3)
+    array
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
+  array
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+  array.each { |x| x.reverse! }
+  array.sort!
+  array.each { |x| x.reverse! }
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  if string.length % 2 == 0
+    string.slice(0..(string.length / 2) - 1)
+  else
+    string.slice(0..(string.length / 2))
+  end
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  number = -(number.abs)
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
@@ -56,6 +81,13 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  seperated = Array.new
+  odds = Array.new
+  evens = Array.new
+  array.each { |x| x % 2 == 0 ? evens.push(x) : odds.push(x)}
+  seperated.push(evens)
+  seperated.push(odds)
+  seperated
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -63,33 +95,42 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  count = 0
+  array.each { |x| x == x.reverse ? count += 1 : nil }
+  count
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.min_by(&:length)
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.max_by(&:length)
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.sum
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array + array
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  (array.sum / array.length.to_f).round
 end
 
 # get all the elements in an array, up until the first element
@@ -97,12 +138,16 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+  new_array = Array.new
+  array.each { |x| x > 5 ? break : new_array.push(x) }
+  new_array
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  Hash[*array]
 end
 
 # get all the letters used in an array of words and return
@@ -110,43 +155,58 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  string = ""
+  array.each { |x| string += x }
+  letters = string.split('')
+  letters.sort
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  total = 0
+  hash.each { |key, value| total += key + value}
+  total
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+  letters = string.split('')
+  letters.delete_if { |x| x == x.upcase && x != ' ' }
+  letters.join('')
 end
 
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+  float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+  date.strftime('%d/%m/%Y')
 end
 
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  email.gsub(/.+@([^.]+).+/, '\1')
 end
 
 # capitalize the first letter in each word of a string,
@@ -155,30 +215,51 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  words = string.split(' ')
+  words.each do |x|
+    if x == 'a' || x == 'and' || x == 'the'
+      x
+    else
+      x.capitalize!
+    end
+  end
+  words[0].capitalize!
+  string = words.join(' ')
 end
 
 # return true if a string contains any special characters
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+  string.index( /[^[:alnum:]]/ ) != nil
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+  range.last
 end
 
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  range.exclude_end?
 end
 
 # get the square root of a number
 def square_root_of(number)
+  Math.sqrt(number)
 end
 
 # count the number of words in a file
 def word_count_a_file(file_path)
+  file = File.open(file_path, "r")
+  count = 0
+  file.each_line do |line|
+    words = line.split
+    count += words.length
+  end
+  count
 end
 
 # --- tougher ones ---
@@ -187,12 +268,17 @@ end
 # called call_method_from_string('foobar')
 # the method foobar should be invoked
 def call_method_from_string(str_method)
+  send(str_method)
 end
 
 # return true if the date is a uk bank holiday for 2014
 # the list of bank holidays is here:
 # https://www.gov.uk/bank-holidays
 def is_a_2014_bank_holiday?(date)
+  bank_holidays = [Time.new(2014, 1, 1), Time.new(2014, 4, 18),
+  Time.new(2014, 4, 21),Time.new(2014, 5, 5),Time.new(2014, 5, 26),
+Time.new(2014, 8, 25),Time.new(2014, 12, 25),Time.new(2014, 12, 26)]
+bank_holidays.include?(date)
 end
 
 # given your birthday this year, this method tells you
@@ -214,6 +300,29 @@ end
 # go from 1 to 100
 # (there's no RSpec test for this one)
 def fizzbuzz_without_modulo
+  fifteens = []
+  fives = []
+  threes = []
+  6.times do |x|
+    fifteens.push(15 * x)
+  end
+  20.times do |x|
+    fives.push(5 * x)
+  end
+  34.times do |x|
+    threes.push(3 * x)
+  end
+  for x in 1..100
+    if fifteens.include?(x)
+      puts "FizzBuzz"
+    elsif fives.include?(x)
+      puts "Buzz"
+    elsif threes.include?(x)
+      puts "Fizz"
+    else
+      puts x
+    end
+  end
 end
 
 # print the lyrics of the song 99 bottles of beer on the wall
@@ -223,4 +332,9 @@ end
 # at the end.
 # (there's no RSpec test for this one)
 def ninety_nine_bottles_of_beer
+  99.downto(2) do |x|
+    puts "#{x} bottles of beer on the wall"
+  end
+  puts "1 bottle of beer on the wall"
+  puts "no more bottles of beer on the wall"
 end
